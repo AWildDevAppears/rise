1e12e46bc4bbb0fd63f2e1da3b08286c64f61cfd
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-11-26 16:38:50 +0000

    Add in tests for running

--- __tests__/pawn.spec.ts
+++ __tests__/pawn.spec.ts
@@ -45,4 +45,48 @@ describe('Pawn', () => {
 
         expect(p.posY).toBe(50 + PAWN_CONSTANTS.moveSpeed);
     });
+
+    it('Should run left', () => {
+        const p = new Pawn();
+
+        p.posX = 50;
+        p.posY = 50;
+
+        p.moveLeft(true);
+
+        expect(p.posX).toBe(50 - PAWN_CONSTANTS.runSpeed);
+    });
+
+    it('Should run right', () => {
+        const p = new Pawn();
+
+        p.posX = 50;
+        p.posY = 50;
+
+        p.moveRight(true);
+
+        expect(p.posX).toBe(50 + PAWN_CONSTANTS.runSpeed);
+    });
+
+    it('Should run down', () => {
+        const p = new Pawn();
+
+        p.posX = 50;
+        p.posY = 50;
+
+        p.moveDown(true);
+
+        expect(p.posY).toBe(50 - PAWN_CONSTANTS.runSpeed);
+    });
+
+    it('Should run up', () => {
+        const p = new Pawn();
+
+        p.posX = 50;
+        p.posY = 50;
+
+        p.moveUp(true);
+
+        expect(p.posY).toBe(50 + PAWN_CONSTANTS.runSpeed);
+    });
 });
