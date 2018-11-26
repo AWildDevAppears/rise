777421e863b4a79002746bc4aa6c5dfb6e47b28c
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-11-26 16:50:55 +0000

    Updates

---
+++ precache-manifest.163253897d681a0df885ebcd761bd449.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "/scripts/vendor.0d9ee707616dbc9134dc.js"
+  },
+  {
+    "url": "/scripts/rise.0d9ee707616dbc9134dc.js"
+  },
+  {
+    "revision": "69ae3484a984a63355112aa731a94030",
+    "url": "/index.html"
+  },
+  {
+    "url": "/css/rise.0d9ee707616dbc9134dc.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+importScripts("/precache-manifest.163253897d681a0df885ebcd761bd449.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+function addIndexRevision(urls, defRev) {
+    const rando = (Math.random() * 1000000).toPrecision(6);
+    const indices = __precacheManifest.filter(m => m.url === '/index.html');
+    const revision = indices.length === 1 ? indices[0].revision : defRev || rando;
+    return urls.map(url => {
+        return { url, revision };
+    });
+}
+
+workbox.core.setCacheNameDetails({
+    prefix: 'rise',
+    suffix: 'v1',
+    precache: 'install-time',
+    runtime: 'run-time',
+    googleAnalytics: 'ga',
+});
+
+workbox.precaching.precache(['/img/yeoman-033.png']);
+
+workbox.precaching.precache(addIndexRevision(['/', '/manifest.json']));
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {
+    cleanUrls: false,
+});
+
