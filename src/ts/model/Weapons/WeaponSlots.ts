c6f6aed6b372c8b7edac7e6979b4b52e9a4b7123
Josh Burgess <joshuacburgess32@gmail.com>
2019-02-03 11:18:07 +0000

    Make all weapon components useful

--- src/ts/model/Weapons/WeaponSlots.ts
+++ src/ts/model/Weapons/WeaponSlots.ts
@@ -6,26 +6,49 @@ export default interface IWeaponSlots {
     stock?: WeaponStock;
     grip?: WeaponGrip;
     magasine?: Magasine;
+    sights?: WeaponSights;
+    barrel?: WeaponBarrel;
 };
 
 export class WeaponComponent extends Item {
     bindTo: string = '';
+    fixed: boolean = false;
 }
 
 export class WeaponMuzzle extends WeaponComponent {
+    bindTo = 'muzzle';
+    range: number = 0;
+    damage: number = 0;
+}
 
+export class WeaponBarrel extends WeaponComponent {
+    bindTo = 'barrel';
+    range: number = 0;
+    accuracy: number = 0;
+    damage: number = 0;
 }
 
 export class WeaponBody extends WeaponComponent {
-
+    bindTo = 'body';
+    damage: number = 0;
+    range: number = 0;
+    accuracy: number = 0;
+    fireRate: number = 0;
 }
 
 export class WeaponStock extends WeaponComponent {
-
+    bindTo = 'stock';
+    accuracy: number = 0;
 }
 
 export class WeaponGrip extends WeaponComponent {
+    bindTo = 'grip';
+    accuracy: number = 0;
+}
 
+export class WeaponSights extends WeaponComponent {
+    bindTo = 'sights';
+    accuracy: number = 0;
 }
 
 export class Magasine extends WeaponComponent {
