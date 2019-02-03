fc011ba1408c9f905f1cf2f520ee7867fe4af222
Josh Burgess <joshuacburgess32@gmail.com>
2019-02-03 11:29:34 +0000

    Add tests to implement

---
+++ __tests__/model/Weapon.test.ts
@@ -0,0 +1,25 @@
+describe('Wepon tests', () => {
+    it('should not let me remove fixed items from weapons (e.g a bows magasine)', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should update stats for a weapon based on the components that have been attacked to it', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should not let me slot a component to a weapon which doesn\'t have a slot for it', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should only let me slot a component to a weapon that works for this type of weapon, even if it has a slot for it', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should unequip a component if I try to replace it', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should consume more bullets from a burst fire weapon, and increase damage accordingly', () => {
+        expect(true).toBe(false);
+    });
+});
