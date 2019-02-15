1e96b7c49ebd1ea9a0802df7e8138f9b72bc7c5c
Josh Burgess <jburgess@whiteoctober.co.uk>
2019-02-15 09:31:36 +0000

    Add new test cases

---
+++ __tests__/model/Factions.test.ts
@@ -0,0 +1,14 @@
+describe('Faction tests', () => {
+    it('Should allow a character to join a fatcion', () => {});
+    it('Should allow a character to be evicted from a faction', () => {});
+    it('Should allow a character to join multiple factions', () => {});
+    it('Should allow a character to gain populatity in a faction', () => {});
+    it('Should allow a character to lose popularity in a faction', () => {});
+    it('Should allow a character to lose popularity in one faction by gaining popularity in another', () => {});
+    it('Should allow the most popular character in a faction to become the leader', () => {});
+    it('Should allow a character to lose popularity if they attack a party member', () => {});
+    it('Should allow a character to gain popularity through fear', () => {});
+    it('Should allow a character to gain popularity through bartering', () => {});
+    it('Should allow a character to gain popularity through charisma', () => {});
+    it('Should allow a feared character to lose popularity by being friendly', () => {});
+});
---
+++ __tests__/model/Modifiers.test.ts
@@ -0,0 +1,14 @@
+describe('Modifier tests', () => {
+    it('Should apply modifers to characters', () => {});
+    it('Should apply modifiers from potions to a character', () => {});
+    it('Should apply modifiers from weapons to a character', () => {});
+    it('Should allow modifiers to override other modifiers', () => {
+        // If I am on fire, and then I get the wet modifier, I should lose the burning modifier
+    });
+    it('Should allow me to get several modifiers', () => {});
+    it('Should take modifiers into account when calculating stats', () => {});
+    it('Should not allow a character to have two modifiers of of the same type', () => {
+        // I can't be on fire twice at the same time.
+    });
+    it('Should remove a modifier from a piece of armor from a character when they unequip it', () => {});
+});
