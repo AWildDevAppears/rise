1dd584e907e042a76bb9cfdefed0c0218a069e46
Josh Burgess <jburgess@whiteoctober.co.uk>
2019-02-21 14:28:15 +0000

    Move container into the right location

--- src/ts/model/Container.ts
+++
@@ -1,6 +0,0 @@
-import Entity from './abstract/Entity';
-import Inventory from './Inventory';
-
-export default class Container extends Entity {
-    inventory = new Inventory(0);
-}
--- src/ts/model/Objects/Container.ts
+++ src/ts/model/Objects/Container.ts
@@ -2,9 +2,11 @@ import Structure from './Structure';
 import Inventory from '../Inventory';
 
 export default class Container extends Structure {
-    inventory: Inventory = new Inventory(12);
+    inventory: Inventory = new Inventory(0);
     
     use() {
+        this.stats.isActive = !this.stats.isActive;
+        
         // open a trade window with this container.
     };
 }
