15598064c76320203a7e9c26ab347389e593df41
Josh Burgess <jburgess@whiteoctober.co.uk>
2019-02-21 11:46:37 +0000

    Add crafting tests

--- __tests__/model/Crafting.test.ts
+++ __tests__/model/Crafting.test.ts
@@ -52,6 +52,51 @@ describe('Crafting', () => {
         // All of the items should have been consumed and only the item to remain
         expect(character.inventory.count()).toBe(1);
     });
-    it('should not remove any of the items if I cannot craft the item', () => {});
-    it('should let a character learn a recipe', () => {});
+    it('should not remove any of the items if I cannot craft the item', () => {
+        const recipe = new Recipe();
+        const result = new Item();
+        const character = new Humanoid();
+
+        const itemOne = new Item();
+        const itemTwo = new Item();
+
+        itemOne.id = 'one';
+        itemTwo.id = 'two';
+
+        recipe.ingredients = {
+            one: 1,
+            two: 2,
+        };
+
+        recipe.output = result;
+
+        // Add items to the inventory, bu not enough to make the item
+        character.inventory.addItem(itemOne);
+        character.inventory.addItem(itemTwo);
+
+        // I've only added two items to the inventory
+        expect(character.inventory.count()).toBe(2);
+
+        // I'm missing one of the "item two"'s I need to craft this item
+        expect(character.craft(recipe)).toBe(false);
+
+        // Nothing is removed from my inventory, I can't craft this item
+        expect(character.inventory.count()).toBe(2);
+    });
+    it('should let a character learn a recipe', () => {
+        const character = new Humanoid();
+        const recipe = new Recipe();
+
+        character.addRecipe(recipe);
+
+        expect(character.recipes.length).toBe(1);
+    });
+
+    it('should not let a character learn a recipe twice', () => {
+        const character = new Humanoid();
+        const recipe = new Recipe('soup');
+
+        expect(character.addRecipe(recipe)).toBe(true);
+        expect(character.addRecipe(recipe)).toBe(false);
+    });
 });
