2657b531ca8132e0a2b1dafa7a909485e0212c6b
Josh Burgess <jburgess@whiteoctober.co.uk>
2019-04-23 15:52:33 +0100

    Add broadcast model for ferrying data

---
+++ src/ts/model/Broadcast.ts
@@ -0,0 +1,13 @@
+export const BROADCASTS = {
+    OPEN_CONTAINER: 'OPEN_CONTAINER',
+}
+
+export default class Broadcast {
+    type: string = '';
+    body: object = {};
+
+    constructor(type: string, data: object) {
+        this.type = type;
+        this.body = data;
+    }
+}
